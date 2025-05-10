f961dc32a23ffedf290e6c1c2fb8ab73a7e18f84
sbslee <sbstevenlee@gmail.com>
2025-05-11 08:50:06 +0900

    Update file

--- streamlit_excel/__init__.py
+++ streamlit_excel/__init__.py
@@ -3,10 +3,9 @@ import streamlit as st
 class Table:
     def __init__(self, df):
         self.df = df
-        self.filters = {}
-        self.select_all = {}
+        self.data = {}
 
-    def add_filter(self, column):
+    def _add_filter(self, column):
         st.markdown("""
             <style>
                 .stCheckbox {
@@ -15,33 +14,59 @@ class Table:
             </style>
         """, unsafe_allow_html=True)
 
-        if column not in self.select_all:
-            self.select_all[column] = True
+        if column not in self.data:
+            self.data[column] = {
+                "select_all_state": True,
+                "selected_options": [],
+                "frozen_options": [],
+            }
 
-        if st.button("Select All"):
-            self.select_all[column] = not self.select_all[column]
+        self.data[column]["frozen_options"] = list(self.view[column].unique())
+
+        displayed_options = self.data[column]["frozen_options"][:]
+        
+        col1, col2 = st.columns(2)
+
+        with col1:
+            if st.button("Select All", key=f"select_all_state_{column}"):
+                self.data[column]["select_all_state"] = not self.data[column]["select_all_state"]
+                
+        with col2:
+            query = st.text_input(
+                "Search",
+                placeholder="Search",
+                label_visibility="collapsed",
+                key=f"search_{column}",
+            )
+
+        if query is not None:
+            displayed_options = [option for option in displayed_options if query.lower() in option.lower()]
 
         with st.form(column, border=False):
-            with st.container(height=200):
-                states = {}
-                for option in self.df[column].unique():
-                    states[option] = st.checkbox(option, value=self.select_all[column])
-            options = [option for option, selected in states.items() if selected]
-            applied = st.form_submit_button(label="Apply Filter")
-            if applied and options:
-                self.filters[column] = options
-            elif applied and not options:
+            with st.container(height=100, border=True):
+                option_states = {}
+                for option in displayed_options:
+                    option_states[option] = st.checkbox(option, value=self.data[column]["select_all_state"])
+            selected_options = [option for option, selected in option_states.items() if selected]
+            filter_applied = st.form_submit_button(label="Apply Filter")
+            if filter_applied and selected_options:
+                self.data[column]["selected_options"] = selected_options
+            elif filter_applied and not selected_options:
                 st.warning("Please select at least one option.")
 
-    def show_filter_panel(self, columns):
+    def show_filter_panel(self, label, columns):
         with st.sidebar:
-            with st.container(border=True):
-                for column in columns:
-                    self.add_filter(column)
+            with st.expander(label):
+                with st.container():
+                    if st.button("Reset Filters"):
+                        self.data = {}
+                    for column in columns:
+                        self._add_filter(column)
 
+    @property
     def view(self):
         df = self.df.copy()
-        for column, options in self.filters.items():
-            if column in df.columns:
-                df = df[df[column].isin(options)]
+        for column, data in self.data.items():
+            if data["selected_options"]:
+                df = df[df[column].isin(data["selected_options"])]
         return df
