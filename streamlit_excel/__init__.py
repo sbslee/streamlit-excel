d786c8b7bb1bdff3a411fc6cc1b1fc58e2f2b870
sbslee <sbstevenlee@gmail.com>
2025-05-11 13:47:07 +0900

    Update file

--- streamlit_excel/__init__.py
+++ streamlit_excel/__init__.py
@@ -5,58 +5,7 @@ class Table:
         self.df = df
         self.data = {}
 
-    def _add_css(self):
-        st.markdown("""
-            <style>
-                /* Reduce font size and internal div size */
-                section[data-testid="stSidebar"] button > div {
-                    font-size: 12px !important;
-                    padding: 2px 8px !important;
-                }
-
-                /* Shrink the button height and spacing */
-                section[data-testid="stSidebar"] button {
-                    padding: 2px 8px !important;
-                    height: auto !important;
-                    min-height: 28px !important;
-                }
-                div[data-testid="stCheckbox"] > label {
-                    transform: scale(0.75);
-                    transform-origin: top left;
-                    display: inline-flex;
-                    align-items: center;
-                }
-                .stCheckbox {
-                    margin-bottom: -25px;
-                }
-                /* Shrink text input and its wrapper */
-                div[data-testid="stTextInput"] {
-                    margin-bottom: 5px !important;
-                    height: 10px !important;
-                }
-
-                div[data-testid="stTextInput"] > div {
-                    height: 28px !important;
-                    padding: 0 !important;
-                }
-
-                div[data-testid="stTextInput"] input {
-                    font-size: 12px !important;
-                    padding: 2px 6px !important;
-                    height: 24px !important;
-                    min-height: 24px !important;
-                }
-
-                /* Placeholder font */
-                ::placeholder {
-                    font-size: 12px !important;
-                }
-            </style>
-        """, unsafe_allow_html=True)
-
     def _add_filter(self, column):
-        self._add_css()
-
         if column not in self.data:
             self.data[column] = {
                 "select_all_state": True,
@@ -65,47 +14,48 @@ class Table:
 
         displayed_options = self.view[column].unique()
 
-        query = st.text_input(
-            "Search",
-            placeholder="Search",
-            label_visibility="collapsed",
-            key=f"search_{column}",
-        )
+        with st.popover(column, use_container_width=True):
+            query = st.text_input(
+                "Search",
+                placeholder="Search",
+                label_visibility="collapsed",
+                key=f"search_{column}",
+            )
 
-        if query is not None:
-            displayed_options = [option for option in displayed_options if query.lower() in option.lower()]
+            if query is not None:
+                displayed_options = [option for option in displayed_options if query.lower() in option.lower()]
 
-        with st.form(column, border=False):
-            with st.container(height=100, border=True):
+            with st.form(column, border=False):
+                col1, col2, col3 = st.columns(3)
+                with col1:
+                    clicked_apply_filter = st.form_submit_button(label="Apply Filter")
+                with col2:
+                    clicked_select_all = st.form_submit_button("Select All")
+                with col3:
+                    clicked_reset_filter = st.form_submit_button("Reset Filter")
                 option_states = {}
-                for option in displayed_options:
-                    option_states[option] = st.checkbox(option, value=self.data[column]["select_all_state"])
-            selected_options = [option for option, selected in option_states.items() if selected]
-
-            col1, col2, col3 = st.columns(3)
-            
-            with col1:
-                filter_applied = st.form_submit_button(label="Apply")
-            with col2:
-                if st.form_submit_button("All"):
+                with st.container(height=200):
+                    for option in displayed_options:
+                        option_states[option] = st.checkbox(option, value=self.data[column]["select_all_state"])
+                selected_options = [option for option, selected in option_states.items() if selected]
+                if clicked_apply_filter and not selected_options:
+                    st.warning("Please select at least one option.")
+                elif clicked_apply_filter and selected_options:
+                    self.data[column]["selected_options"] = selected_options
+                    st.rerun()
+                elif clicked_select_all:
                     self.data[column]["select_all_state"] = not self.data[column]["select_all_state"]
                     st.rerun()
-            with col3:
-                if st.form_submit_button("Clear"):
+                elif clicked_reset_filter:
                     self.data[column]["select_all_state"] = True
                     self.data[column]["selected_options"] = []
                     st.rerun()
-            if filter_applied and selected_options:
-                self.data[column]["selected_options"] = selected_options
-                st.rerun()
-            elif filter_applied and not selected_options:
-                st.warning("Please select at least one option.")
 
     def show_filter_panel(self, label, columns):
         with st.sidebar:
             with st.expander(label):
                 with st.container():
-                    if st.button("Reset"):
+                    if st.button("Reset All Filters"):
                         self.data = {}
                     for column in columns:
                         self._add_filter(column)
