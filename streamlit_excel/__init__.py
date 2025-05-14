6d1b9c939a8ca0b272feb69180299afb8590e987
Seung-been "Steven" Lee <sbstevenlee@gmail.com>
2025-05-14 17:22:51 +0900

    Update file

--- streamlit_excel/__init__.py
+++ streamlit_excel/__init__.py
@@ -86,82 +86,48 @@ class Table:
             icon = None
 
         with st.popover(column, use_container_width=True, icon=icon):
-            tab1, tab2 = st.tabs(["Calendar", "Selection"])
-            with tab1:
-                with st.form(f"{self.key}_{column}_calendar", border=False):
-                    clicked_apply_filter = st.form_submit_button(label="Apply Filter", use_container_width=True)
-                    clicked_reset_filter = st.form_submit_button("Reset Filter", use_container_width=True)
-                    clicked_select_all = st.form_submit_button("Select All", use_container_width=True)
-                    min_date, max_date = self.get_min_max(self.view[column])
-                    selected_range = st.date_input(
-                        "Calendar",
-                        value=(min_date, max_date) if self.data[column]["select_all_state"] else [],
-                        min_value=min_date,
-                        max_value=max_date,
-                        label_visibility="collapsed",
-                    )
-                    if clicked_apply_filter:
-                        if selected_range:
-                            selected_range = pd.date_range(
-                                start=pd.to_datetime(selected_range[0]),
-                                end=pd.to_datetime(selected_range[1])
-                            )
-                            self.data[column]["selected_years"] = selected_range.year.unique().to_list()
-                            self.data[column]["selected_months"] = selected_range.month.unique().to_list()
-                            self.data[column]["selected_days"] = selected_range.day.unique().to_list()
-                            self.reset_cache()
-                        else:
-                            st.warning("Please select a date range.")
-                    elif clicked_reset_filter:
-                        self.data.pop(column)
+            with st.form(f"{self.key}_{column}_selection", border=False):
+                clicked_apply_filter = st.form_submit_button(label="Apply Filter", use_container_width=True)
+                clicked_reset_filter = st.form_submit_button("Reset Filter", use_container_width=True)
+                clicked_select_all = st.form_submit_button("Select All", use_container_width=True)
+                observed_years = np.sort(self.get_unique(self.view[f"{column}_year"]))
+                observed_months = np.sort(self.get_unique(self.view[f"{column}_month"]))
+                observed_days = np.sort(self.get_unique(self.view[f"{column}_day"]))
+                selected_years = st.multiselect(
+                    "Years",
+                    options=observed_years,
+                    default=self.data[column]["selected_years"] if self.data[column]["selected_years"] else observed_years if self.data[column]["select_all_state"] else None,
+                    placeholder="YYYY",
+                    label_visibility="collapsed",
+                )
+                selected_months = st.multiselect(
+                    "Months",
+                    options=observed_months,
+                    default=self.data[column]["selected_months"] if self.data[column]["selected_months"] else observed_months if self.data[column]["select_all_state"] else None,
+                    placeholder="MM",
+                    label_visibility="collapsed",
+                )
+                selected_days = st.multiselect(
+                    "Days",
+                    options=observed_days,
+                    default=self.data[column]["selected_days"] if self.data[column]["selected_days"] else observed_days if self.data[column]["select_all_state"] else None,
+                    placeholder="DD",
+                    label_visibility="collapsed",
+                )
+                if clicked_apply_filter:
+                    if selected_years or selected_months or selected_days:
+                        self.data[column]["selected_years"] = selected_years
+                        self.data[column]["selected_months"] = selected_months
+                        self.data[column]["selected_days"] = selected_days
                         self.reset_cache()
-                    elif clicked_select_all:
-                        self.data[column]["select_all_state"] = not self.data[column]["select_all_state"]
-                        st.rerun()
-            with tab2:
-                with st.form(f"{self.key}_{column}_selection", border=False):
-                    clicked_apply_filter = st.form_submit_button(label="Apply Filter", use_container_width=True)
-                    clicked_reset_filter = st.form_submit_button("Reset Filter", use_container_width=True)
-                    clicked_select_all = st.form_submit_button("Select All", use_container_width=True)
-                    observed_years = np.sort(self.get_unique(self.view[f"{column}_year"]))
-                    observed_months = np.sort(self.get_unique(self.view[f"{column}_month"]))
-                    observed_days = np.sort(self.get_unique(self.view[f"{column}_day"]))
-                    selected_years = st.multiselect(
-                        "Years",
-                        options=observed_years,
-                        default=self.data[column]["selected_years"] if self.data[column]["selected_years"] else observed_years if self.data[column]["select_all_state"] else None,
-                        placeholder="YYYY",
-                        label_visibility="collapsed",
-                    )
-                    selected_months = st.multiselect(
-                        "Months",
-                        options=observed_months,
-                        default=self.data[column]["selected_months"] if self.data[column]["selected_months"] else observed_months if self.data[column]["select_all_state"] else None,
-                        placeholder="MM",
-                        label_visibility="collapsed",
-                    )
-                    print(self.data[column]["selected_days"], observed_days)
-                    selected_days = st.multiselect(
-                        "Days",
-                        options=observed_days,
-                        default=self.data[column]["selected_days"] if self.data[column]["selected_days"] else observed_days if self.data[column]["select_all_state"] else None,
-                        placeholder="DD",
-                        label_visibility="collapsed",
-                    )
-                    if clicked_apply_filter:
-                        if selected_years or selected_months or selected_days:
-                            self.data[column]["selected_years"] = selected_years
-                            self.data[column]["selected_months"] = selected_months
-                            self.data[column]["selected_days"] = selected_days
-                            self.reset_cache()
-                        else:
-                            st.warning("Please select at least one option.")
-                    elif clicked_reset_filter:
-                        self.data.pop(column)
-                        self.reset_cache()
-                    elif clicked_select_all:
-                        self.data[column]["select_all_state"] = not self.data[column]["select_all_state"]
-                        st.rerun()
+                    else:
+                        st.warning("Please select at least one option.")
+                elif clicked_reset_filter:
+                    self.data.pop(column)
+                    self.reset_cache()
+                elif clicked_select_all:
+                    self.data[column]["select_all_state"] = not self.data[column]["select_all_state"]
+                    st.rerun()
 
     def show_filter_panel(self, label, columns):
         with st.sidebar:
